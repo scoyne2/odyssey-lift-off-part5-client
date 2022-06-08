1a9b20d834f5e3dba4a0033e22755284386138ec
Sean Coyne <scoyne@goodrx.com>
2022-06-07 18:07:17 -0700

    Commit url

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+  uri: 'https://scoyne-app.herokuapp.com/',
+  cache: new InMemoryCache()
 });
+
 
 ReactDOM.render(
   <ApolloProvider client={client}>
